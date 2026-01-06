b273fcf8bd5f0d5b25ee5c68d2d8ae7dcc6f18ce
zenstorage <bielhenriquek.k@gmail.com>
2026-01-06 18:44:55 -0300

    up

--- main.ts
+++ main.ts
@@ -1,9 +1,41 @@
-import { serveFile } from "jsr:@std/http/file-server";
+Deno.serve(async (req) => {
+  const url = new URL(req.url);
+  const path = url.pathname;
 
-Deno.serve((req: Request) => {
-    const res = fetch("https://checkip.amazonaws.com/");
+  if (path === "/ip") {
+    const ip = await getIP();
+    return new Response(ip, {
+      headers: { "Content-Type": "text/plain" },
+    });
+  }
 
-    return res;
+  if (path === "/hw") {
+    return new Response("Hello World!", {
+      headers: { "Content-Type": "text/plain" },
+    });
+  }
 
-    // return serveFile(req, "./index.html");
+  if (path === "/debug") {
+    const debug = {
+      status: "ok",
+      method: req.method,
+      headers: Object.fromEntries(req.headers),
+      url: req.url,
+    };
+
+    return new Response(JSON.stringify(debug, null, 2), {
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+
+  return new Response("Not Found", {
+    status: 404,
+    headers: { "Content-Type": "text/plain" },
+  });
 });
+
+async function getIP(): Promise<string> {
+  const res = await fetch("https://api.ipify.org?format=json");
+  const data = await res.json();
+  return data.ip;
+}
