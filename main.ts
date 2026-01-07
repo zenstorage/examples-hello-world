46be45bf6a57855df20ca3459e92bfe89a909fb9
zenstorage <bielhenriquek.k@gmail.com>
2026-01-07 11:24:27 -0300

    up

--- main.ts
+++ main.ts
@@ -35,7 +35,7 @@ Deno.serve(async (req) => {
   }
 
   const id = path.substring(1);
-  const pdURL = `https://pixeldrain.com/api/file/${id}?download`;
+  const pdURL = `https://pixeldrain.com/api/file/${id}${url.searchParams.has("download") ? "?download" : ""}`;
   const res = fetch(pdURL);
 
   return res;
